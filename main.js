7913feddacae5e514b00be0f69ee38f66ea209e0
olaforzdebski <olaforzdebski@onet.pl>
2021-03-14 20:17:46 +0100

    Modified main.js (Vue)

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
+            product: 'Socks',
+            details: ['20% polyster', '30% wool', '50% cotton'],
+            variants: [
+                {id: 1, color: 'blue', image: './assets/socks_blue.jpg'},
+                {id: 2, color: 'green', image: './assets/socks_green.jpg'}
+            ],
+            inStock: false,
+            image: '/assets/socks_blue.jpg'
         }
     }, 
     methods: {
         addToCart() {
             this.cart++
+        },
+        changeImage(x) {
+            this.image = x
         }    
-    },
+    }
 })
